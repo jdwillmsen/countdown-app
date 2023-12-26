d1b36e2e21e10e78c6164aac2b28425259a0dfa1
Jacob Willmsen <jdwillmsen@gmail.com>
2023-12-26 13:46:58 -0600

    Updating pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -27,9 +27,54 @@ export default defineConfig({
         enabled: true,
       },
       manifest: {
+        name: 'Boys Weekend Countdown',
+        short_name: 'Boys Weekend',
+        theme_color: '#00ff00',
         icons: [
           {
-            src: '/icons/512.png',
+            src: 'assets/icons/icon-72.png',
+            sizes: '72x72',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: 'assets/icons/icon-96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: 'assets/icons/icon-128.png',
+            sizes: '128x128',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: 'assets/icons/icon-144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: 'assets/icons/icon-152.png',
+            sizes: '152x152',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: 'assets/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: 'assets/icons/icon-384.png',
+            sizes: '384x384',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: 'assets/icons/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
