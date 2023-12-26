297da54ef1c64c0c6ccd00f6a7dff5d318ce0887
Jacob Willmsen <jdwillmsen@gmail.com>
2023-12-25 22:21:59 -0600

    Updated background image file type

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,6 @@
 import styles from './app.module.scss';
 import CountdownTimer from './countdown-timer/countdown-timer';
-import background from '../assets/camp_background.jpg';
+import background from '../assets/camp_background.webp';
 
 export function App() {
   const boysWeekendDate = new Date('07/18/2024 18:00:00');
